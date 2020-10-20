50c895a1aa5dcba127f2f962dfaee34ee2f69fd2
Bhaktisetiawan <muhammadbhaktisetiawan@gmail.com>
2020-10-20 22:05:38 +0700

    Update script_dashboard_subsurf_uir.R

--- script_dashboard_subsurf_uir.R
+++ script_dashboard_subsurf_uir.R
@@ -1,33 +1,63 @@
-#==================WD======================
-#Setting working directory (folder kerja)
-getwd() #menampilkan working directory saat ini
+#====Setting folder location====
+getwd() #get working directory
 setwd("D:/3. Datascience/1. R Studio/3. Project R/dashboard_subsurf_uir")
 
-#------------------Input-------------------
-#Memasukkan dataset kedalam R
-##Pastikan tipe file nya apa?
+#Membaca data CSV equinor
+#Manual
+#Otomatis
 
-volve <- read.csv("PROD_EQUINOR.csv")
+volve <- read.csv("D:/3. Datascience/1. R Studio/3. Project R/dashboard_subsurf_uir/PROD_EQUINOR.csv")
 
-#------------------Manipulation------------
+#==============Melihat data gimana============
+str(volve)
+summary(volve)
+head(volve)
+tail(volve)
+names(volve)
+
+#==========indexing===============
+a <- volve[1:5,1:5]
+
+#==========merubah date============
 library(lubridate)
-volve$date <- dmy(volve$DATEPRD)
+a$date <- dmy(a$DATEPRD)
+a <- select(a,date,everything()) 
+a <- select(a,-DATEPRD)
 
-#A little bit data manipulations
-library(magrittr)
-volve$WC <- volve$BORE_WAT_VOL / (volve$BORE_OIL_VOL + volve$BORE_WAT_VOL) # watercut
-volve$GOR <- volve$BORE_GAS_VOL/volve$BORE_OIL_VOL # GOR
+colnames(a)[2] <- "well"
+  
+#============Data processing=============
+library(dplyr)
+#select
+a <- select(volve,c(DATEPRD,BORE_OIL_VOL,BORE_GAS_VOL,BORE_WAT_VOL))
+a <- volve[,c(1,4,16:24)]
 
-#---------------Plotting-------------------
-#Plotting
+#filtering
+a <- filter(a, BORE_WAT_VOL>=0)
+
+#Arange
+a <- arrange(a,NPD_WELL_BORE_NAME,DATEPRD)
+a$year <- year(a$date)
+
+#summary #mutate
+library(magrittr) #pipe
+
+a.yr <- a %>% group_by(year,well) %>% summarise(NPOIL=sum(BORE_OIL_VOL),
+                                                NPGAS=sum(BORE_GAS_VOL),
+                                                NPWAT=sum(BORE_WAT_VOL))
+
+#==========Plotting===================
 library(ggplot2)
+library(patchwork)
 
-ggplot() + geom_point(data = volve, aes(date,BORE_OIL_VOL,color=WELL_BORE_CODE)) + theme_bw()
-ggplot() + geom_point(data = volve, aes(date,AVG_DOWNHOLE_PRESSURE,color=WELL_BORE_CODE)) + theme_bw()
-ggplot() + geom_line(data = volve, aes(date,WC,color=WELL_BORE_CODE)) + theme_bw() + coord_cartesian(ylim = c(0,1)) + facet_wrap(~WELL_BORE_CODE)
-ggplot() + geom_line(data = volve, aes(date,GOR,color=WELL_BORE_CODE)) + theme_bw()
+p1<-ggplot() + geom_point(data = a,aes(date,BORE_OIL_VOL,color=well))+
+  ggtitle("OIL PROD DAILY") + theme_bw()
 
-#Tambahan untuk data welltesting
+p2<-ggplot() + geom_line(data = a.yr, aes(year,NPOIL))
 
+p3 <- ggplot(data = a.yr, aes(year,NPOIL,fill=well)) + geom_bar(stat = 'identity') + ggtitle("kontribusi tahunan")
 
-#drilling
+p4<-ggplot(data = a.yr, aes(x='', y=NPOIL, fill=well)) + geom_bar(stat = 'identity') +coord_polar('y',start = 0)
+
+#menggabungkan
+p1|p2/(p3|p4)
